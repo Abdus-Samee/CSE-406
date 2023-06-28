0c46e8e21a6f00b0726cbcec50d6b37f2c127fe2
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2023-06-28 19:03:49 +0600

    Add files via upload

---
+++ 1805021_f3.py
@@ -0,0 +1,63 @@
+import socket
+from AES_1805021 import inputValidation, key_scheduling, encrypt 
+from Diffie_Hellman_1805021 import generateLargePrime, findGeneratorInRange, modularExponentiation, computeSharedKey
+from Diffie_Hellman_1805021 import mn, mx
+
+# Alice -> Client
+def ALICE():
+    host = "localhost"
+    port = 6236
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+
+        k = int(input("Enter bit length: "))
+        p = generateLargePrime(k)
+        g = findGeneratorInRange(p, mn[k], mx[k])
+        a = generateLargePrime(k//2)
+        A = modularExponentiation(g, a, p)
+
+        p = str(p)
+        g = str(g)
+        A = str(A)
+        print("Generated g:", g)
+        print("Generated p:", p)
+        print("Generated A:", A)
+
+        data = p + " " +  g + " " + A
+        s.sendall(data.encode())
+        # s.sendall(p.encode())
+        # s.sendall(g.encode())
+
+        B = s.recv(1024).decode()
+        B = int(B)
+        print("Received B from Bob:", B)
+
+        p = int(p)
+        shared_key = computeSharedKey(B, a, p)
+        hex_key = str(shared_key).encode("utf-8").hex()
+        # print("Generated shared key:", hex_key)
+
+        # s.sendall("done".encode())
+        # s.recv(1024).decode()
+
+        # AES encryption
+        plain_text = input("Enter plain text: ")
+        hex_plain_text = plain_text.encode("utf-8").hex()
+
+        hex_plain_text_arr, hex_key = inputValidation(hex_plain_text, hex_key)
+        print("Key after validation:", hex_key)
+        key_list = key_scheduling(hex_key)
+        # print("Generated key list:", key_list[5])
+
+        cipher_text = encrypt(hex_plain_text_arr, key_list)
+        print("Generated cipher Text in HEX:", cipher_text)
+
+        s.sendall(cipher_text.encode())
+
+        s.close()
+
+    print("Connection closed")
+
+
+ALICE()
---
+++ 1805021_f4.py
@@ -0,0 +1,57 @@
+import socket
+from AES_1805021 import inputValidation, key_scheduling, decrypt 
+from Diffie_Hellman_1805021 import generateLargePrime, modularExponentiation, computeSharedKey
+
+def BOB():
+    host = "localhost"
+    port = 6236
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen(1)
+
+        client_socket, address = s.accept()
+
+        data = client_socket.recv(1024).decode().split(" ")
+        # print("Received data", data)
+        p = data[0]
+        g = data[1]
+        A = data[2]
+        k  = len(p)
+        print("Received p, g, and A from Alice:", p, g, A)
+
+        p = int(p)
+        g = int(g)
+        A = int(A)
+        b = generateLargePrime(k//2)
+
+        B = modularExponentiation(g, b, p)
+        B = str(B)
+        print("Generated B:", B)
+        client_socket.sendall(B.encode())
+
+        shared_key = computeSharedKey(A, b, p)
+        hex_key = str(shared_key).encode("utf-8").hex()
+        # print("Generated shared key:", hex_key)
+
+        # client_socket.recv(1024).decode()
+        # s.sendall("done".encode())
+
+        cipher_text = client_socket.recv(1024).decode()
+        print("Received cipher text from Alice:", cipher_text)
+
+        hex_key = inputValidation("", hex_key)[1]
+        print("Key after validation:", hex_key)
+        key_list = key_scheduling(hex_key)
+        # print("Generated key list:", key_list[5])
+
+        decipher_text = decrypt(cipher_text, key_list)
+
+        print("Decipher Text in HEX:", decipher_text)
+        print("Decipher Text in ASCII:", bytearray.fromhex(decipher_text).decode('unicode-escape'))
+
+        s.close()
+    
+    print("Connection closed")
+
+BOB()
